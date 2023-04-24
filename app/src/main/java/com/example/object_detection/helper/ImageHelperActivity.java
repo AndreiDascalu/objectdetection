16b39f958d43494e9b55083e8aafb6f99f4cf33b
AndreiDascalu <dascaluandrei172001@gmail.com>
2023-04-24 14:30:35 +0200

    Image adding functionality

--- app/src/main/java/com/example/object_detection/helper/ImageHelperActivity.java
+++ app/src/main/java/com/example/object_detection/helper/ImageHelperActivity.java
@@ -1,6 +1,7 @@
 package com.example.object_detection.helper;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -8,56 +9,87 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.object_detection.MainActivity;
 import com.example.object_detection.R;
 
+import java.io.IOException;
+
 public class ImageHelperActivity extends AppCompatActivity {
-Button cameraButton;
-Button galleryButton;
+    private ImageView inputImageView;
+    private TextView outputTextView;
+
+    private int REQUEST_PICK_IMAGE = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_helper);
+
+        inputImageView = findViewById(R.id.imageViewInput);
+        outputTextView = findViewById(R.id.textViewOutput);
+
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
             if(ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED){
                 ActivityCompat.requestPermissions(ImageHelperActivity.this,new String[]{Manifest.permission.READ_MEDIA_IMAGES},100);
             }
         }
 
-        galleryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                galleryHelper();
-            }
-        });
-
-//        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.R){
-//                    if(!Environment.isExternalStorageManager()){
-//                        try{
-//                            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-//                            intent.addCategory("android.intent.category.DEFAULT");
-//                            intent.setData(Uri.parse(String.format("package:%s",getApplicationContext().getPackageName())));
-//                            startActivityIfNeeded(intent,101);
-//                        }catch(Exception e){
-//                            Intent intent  =new Intent();
-//                            intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-//                            startActivityIfNeeded(intent,101);
-//                        }
-//                    }
-//                }
     }
 
-    public void galleryHelper(){
-        Intent intent = new Intent();
+    public void onPickImage(View view){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+
+        startActivityForResult(intent,REQUEST_PICK_IMAGE);
+    }
+
+    public void onStartCamera(View view){
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK){
+            if(requestCode==REQUEST_PICK_IMAGE){
+                Uri uri = data.getData();
+                Bitmap bitmap = loadFromUri(uri);
+                inputImageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private Bitmap loadFromUri(Uri uri){
+        Bitmap bitmap = null;
+
+        try{
+            if(Build.VERSION.SDK_INT>Build.VERSION_CODES.O_MR1){
+                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), uri);
+                bitmap = ImageDecoder.decodeBitmap(source);
+            }else{
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+        return bitmap;
     }
 
     @Override
